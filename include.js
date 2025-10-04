ef35316c84aca0e6e6d888fb444015260323a52e
Esteban Bedoya <estebanbzuluaga@gmail.com>
2025-10-04 03:37:20 -0500

    lazyly committing all changes...

--- include.js
+++ include.js
@@ -1,10 +1,9 @@
-document.querySelectorAll("[data-include]")
-	.forEach(e => {
-		fetch(`/partials/${e.dataset.include}.html`)
-			.then(res => res.text())
-			.then(text => {
-				e.outerHTML = text
-			}).catch(err => {
-				console.log(`ERROR WHILE FETCHING /partials/${e.dataset.include}.html\n` + err)
-			})
-	})
+document.querySelectorAll("[data-include]").forEach(e => {
+	fetch(`/partials/${e.dataset.include}.html`)
+		.then(res => res.text())
+		.then(text => {
+			e.outerHTML = text
+		}).catch(err => {
+			console.log(`ERROR WHILE FETCHING /partials/${e.dataset.include}.html\n` + err)
+		})
+})
