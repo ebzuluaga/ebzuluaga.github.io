f2fa086da73abb1c836ceb9499fa272837a869ee
Esteban Bedoya <estebanbedoyazuluaga@gmail.com>
2026-05-12 04:10:13 -0500

    lazyly committing all changes...

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,13 +1,12 @@
 export default function (eleventyConfig) {
 	// set better directory names for source and destination
-	eleventyConfig.setInputDirectory('src');
-	eleventyConfig.setOutputDirectory('dist');
+	eleventyConfig.setInputDirectory("src");
+	eleventyConfig.setOutputDirectory("dist");
 
 	// copy necessary non-template files
-	eleventyConfig.addPassthroughCopy({'src/public': '/'})
-
+	eleventyConfig.addPassthroughCopy({ "src/public": "/" });
 }
 export const config = {
-	markdownTemplateEngine: 'njk',
-	htmlTemplateEngine: 'njk',
-}
+	markdownTemplateEngine: "njk",
+	htmlTemplateEngine: "njk",
+};
